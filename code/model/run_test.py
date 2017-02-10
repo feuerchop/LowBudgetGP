76d240e79122fa998779241626f5830794225e8c
Bojan Kolosnjaji <kolosnjaji@sec.in.tum.de>
2017-02-10 17:43:01 +0100

    synthetic data test

--- code/model/run_test.py
+++ code/model/run_test.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import log_loss
 from optimization_no_annotations import GenGradDescModelNoAnnotations
 import sys
+import scipy.io as sio
 
 
 def load_data_uai(data_i):
@@ -29,6 +30,74 @@ def load_data_uai(data_i):
     my_gt = np.vstack(groundtruth_list)
 
     return my_data, my_labels, my_gt
+
+def load_data_synthetic():
+    data_in = sio.loadmat('../../data/data_in.mat')
+    num_clients = np.size(data_in['Y_sigmoid'],2)
+    all_data = []
+    all_data.append(data_in['Xt']) # x
+    all_data.append(data_in['Z_sigmoid']) # ground truth
+    all_data.append([]) # annotation indices
+    all_data.append([])  # annotations
+    for i in range(num_clients):
+        all_data[2].append(range(0,len(data_in['Xt'])))
+        all_data[3].append(data_in['Y_sigmoid'][:,:,i])
+
+    return all_data
+
+def divide_indices_sythetic(all_data, num_cv=2):
+    num_data = np.size(all_data[0], 0)
+    divide_indices = divide_indices_uai(num_cv, num_data)
+    return divide_indices
+
+def divide_data_synthetic(all_data, cv_indices, test_i):
+    cv_division = cv_indices[test_i]
+    training_indices = cv_division[0]
+    test_indices = cv_division[1]
+
+    X_training = all_data[0][training_indices,:] #my_data[training_indices, :]
+    y_training = all_data[1][training_indices] #my_gt[training_indices]
+    y_training_label_indices = []  # num_clientsx list of indices
+    y_training_label_values = []  # num_clientsxlist of labels
+    y_training_labels = np.squeeze(np.array(all_data[3]))[:, training_indices].T #my_labels[training_indices, :]
+
+    num_clients = y_training_labels.shape[1]
+    num_labels = y_training_labels.shape[0]
+
+    for c in range(num_clients):
+        y_list = []
+        y_indices = []
+        for l in range(num_labels):
+            if (y_training_labels[l, c] != -100):  # label exists
+                y_list.append(y_training_labels[l, c])
+                y_indices.append(l)
+        y_training_label_values.append(y_list)
+        y_training_label_indices.append(y_indices)
+
+    X_test = all_data[0][test_indices, :]
+    y_test = all_data[1][test_indices]
+    y_test_label_indices = []  # num_clientsx list of indices
+    y_test_label_values = []  # num_clientsxlist of labels
+    y_test_labels = np.squeeze(np.array(all_data[3]))[:, test_indices].T
+
+    num_labels = y_test_labels.shape[0]
+
+    for c in range(num_clients):
+        y_list = []
+        y_indices = []
+        for l in range(num_labels):
+                y_list.append(y_test_labels[l, c])
+                y_indices.append(l)
+        y_test_label_values.append(y_list)
+        y_test_label_indices.append(y_indices)
+
+    train_data = [X_training, y_training, y_training_label_indices, y_training_label_values]
+    test_data = [X_test, y_test, y_test_label_indices, y_test_label_values]
+    #return (train_data, test_data)
+
+    return [train_data, test_data]
+
+
 
 def divide_indices_uai(num_cv=2, num_data=3220):
 
@@ -209,6 +278,37 @@ def test_clustering():
         plt.plot(range(num_clients_min, min_num_clients+1), mean_errors_cv, '.-')
         plt.savefig('plot_cv_{0}.png'.format(data_number))
 
+def test_synthetic(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, method):
+
+    all_data = load_data_synthetic()
+    num_cv = 2
+    cv_indices = divide_indices_sythetic(all_data, num_cv)
+    loss_cv = []
+    num_clients = len(all_data[2])
+    for test_i in range(num_cv):
+        print "Test {0}".format(test_i)
+        training_data, test_data = divide_data_synthetic(all_data, cv_indices, test_i)
+
+        training_data[1] = np.where(training_data[1]>0.5, 1, 0)
+
+        for cl_i in range(num_clients):
+            training_data[3][cl_i] = np.where(np.array(training_data[3][cl_i]) > 0.5, 1, 0)
+
+        [n, m] = all_data[0].shape
+        random_v = np.random.normal(1, 0.5, num_clients) / num_clients
+        random_v = random_v / np.sum(random_v)
+        random_w = np.random.normal(0, 0.05, (num_clients, m))
+        train_model = GenGradDescModelNoAnnotations(random_w, random_v)
+        print "******************TRAINING******************"
+        train_model.optimization(training_data[0], training_data[1], training_data[2], training_data[3], NUM_IT=1200,
+                                 NUM_IT_P=NUM_IT_P, PARAM_LAMBDA_W=PARAM_LAMBDA_W,
+                                 PARAM_LAMBDA_ANNOTATIONS=PARAM_LAMBDA_ANNOTATIONS, PARAM_LAMBDA=PARAM_LAMBDA,
+                                 TIMESTEP=TIMESTEP, method=method)
+        print "******************TESTING******************"
+        loss_cv.append(train_model.test(test_data[0], test_data[1], test_data[2], test_data[3], PARAM_LAMBDA_ANNOTATIONS, method))
+        print loss_cv[test_i]
+    print np.mean(loss_cv)
+
 
 def test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, method):
     np.random.seed(42)
@@ -221,6 +321,9 @@ def test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_L
 
         indices = divide_indices_uai(cv_number, np.size(X,0))
         loss_cv = []
+
+        print "TEST: {0}".format(data_number+1)
+
         for i in range(cv_number):
 
             (training_data, test_data) = divide_data_uai_no_annotations(X,L,Y,indices,i)
@@ -238,45 +341,63 @@ def test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_L
 
 if __name__=="__main__":
 
-    test = 'logreg'
+    test = 'synthetic'
 
-    if (test == 'logreg_all'):
-        print "Testing all parameter configurations, takes some days"
+    if (test == 'real'):
 
-        PARAM_LAMBDA_ANNOTATIONS_A = [0.01, 0.1, 1 ]
-        NUM_IT_P_A = [1, 5, 10]
-        PARAM_LAMBDA_W_A = [0.000001, 0.00001, 0.0001, 0.001]
-        TIMESTEP_A = [0.00001, 0.000001, 0.0000001, 0.00000001]
-        PARAM_LAMBDA_A = [0.00001, 0.0001, 0.001, 0.01]
+        method = 'logreg'
 
-        for PARAM_LAMBDA_ANNOTATIONS in PARAM_LAMBDA_ANNOTATIONS_A:
-            for NUM_IT_P in NUM_IT_P_A:
-                for PARAM_LAMBDA_W in PARAM_LAMBDA_W_A:
-                    for TIMESTEP in TIMESTEP_A:
-                        for PARAM_LAMBDA in PARAM_LAMBDA_A:
-                            sys.stdout = open('{0}_{1}_{2}_{3}_{4}.txt'.format(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA, TIMESTEP, PARAM_LAMBDA), 'w')
-                            test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA)
+        if (method == 'logreg_all'):
+            print "Testing all parameter configurations, takes some days"
 
-    elif test== 'logreg':
-        print "Fast test  of logreg with manually assigned parameters"
+            PARAM_LAMBDA_ANNOTATIONS_A = [0.01, 0.1, 1 ]
+            NUM_IT_P_A = [1, 5, 10]
+            PARAM_LAMBDA_W_A = [0.000001, 0.00001, 0.0001, 0.001]
+            TIMESTEP_A = [0.00001, 0.000001, 0.0000001, 0.00000001]
+            PARAM_LAMBDA_A = [0.00001, 0.0001, 0.001, 0.01]
 
-        PARAM_LAMBDA_ANNOTATIONS = 1000
-        NUM_IT_P = 10
-        PARAM_LAMBDA_W = 0.0001
-        TIMESTEP = 0.0001
-        PARAM_LAMBDA = 0.00001
-        test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'LOGREG')
+            for PARAM_LAMBDA_ANNOTATIONS in PARAM_LAMBDA_ANNOTATIONS_A:
+                for NUM_IT_P in NUM_IT_P_A:
+                    for PARAM_LAMBDA_W in PARAM_LAMBDA_W_A:
+                        for TIMESTEP in TIMESTEP_A:
+                            for PARAM_LAMBDA in PARAM_LAMBDA_A:
+                                sys.stdout = open('{0}_{1}_{2}_{3}_{4}.txt'.format(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA, TIMESTEP, PARAM_LAMBDA), 'w')
+                                test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA)
 
-    elif test=='mlp':
-        print "Fast test  of mlp with manually assigned parameters"
+        elif method== 'logreg':
+            print "Fast test  of logreg with manually assigned parameters"
 
-        PARAM_LAMBDA_ANNOTATIONS = 1000
+            PARAM_LAMBDA_ANNOTATIONS = 1000
+            NUM_IT_P = 10
+            PARAM_LAMBDA_W = 0.0001
+            TIMESTEP = 0.0001
+            PARAM_LAMBDA = 0.00001
+            test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'LOGREG')
+
+        elif method=='mlp':
+            print "Fast test  of mlp with manually assigned parameters"
+
+            PARAM_LAMBDA_ANNOTATIONS = 1000
+            NUM_IT_P = 10
+            PARAM_LAMBDA_W = 0.001
+            TIMESTEP = 0.0001
+            PARAM_LAMBDA = 0.00001
+            test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'MLP')
+
+        else:
+            print "Please pick logreg or mlp"
+
+    elif (test == 'synthetic'):
+        PARAM_LAMBDA_ANNOTATIONS = 100000
         NUM_IT_P = 10
         PARAM_LAMBDA_W = 0.001
-        TIMESTEP = 0.0001
+        TIMESTEP = 0.00001
         PARAM_LAMBDA = 0.00001
-        test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'MLP')
+        # test_optimization_no_annotations(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'LOGREG')
+
+        test_synthetic(PARAM_LAMBDA_ANNOTATIONS, NUM_IT_P, PARAM_LAMBDA_W, TIMESTEP, PARAM_LAMBDA, 'LOGREG')
+
+
 
     else:
-        print "Please pick logreg or mlp"
-
+        print "Select a test!"
